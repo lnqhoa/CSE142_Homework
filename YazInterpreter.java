39082f962e2738a179dd795fa8f65b10b532eff0
lnqhoa <46456337+lnqhoa@users.noreply.github.com>
2020-02-26 22:15:09 -0800

    Update YazInterpreter.java

--- YazInterpreter.java
+++ YazInterpreter.java
@@ -15,10 +15,12 @@ import java.util.*;
 import java.io.*;
 
 public class YazInterpreter {
-   public static void main(String[]args) throws FileNotFoundException { 
+   public static void main(String[]args) throws FileNotFoundException {
+      // Setting up Scanner and output file
       Scanner console = new Scanner(System.in);
-      
+
       intro();
+      // Prompting users for the function they want to use
       String answer = prompt(console);
       
       // Answer is not a case-insensitive version of "C", "I" or "Q"
@@ -32,60 +34,66 @@ public class YazInterpreter {
          System.out.println("YazInteractions session. Type END to exit.");
       }
       
-      // Answer is a case-insensitive version of "C", "I" or "Q" 
+      // Answer is a case-insensitive version of "C" 
       while (answer.equalsIgnoreCase("c") || answer.equalsIgnoreCase("i") ||
-             answer.equalsIgnoreCase("q")) {
-         // Answer is a case-insensitive version of "C"
+            answer.equalsIgnoreCase("q")) {
          if (answer.equalsIgnoreCase("c")) {
             String function = console.nextLine();
             // Use convert(String value) function to convert temp
             if (function.toLowerCase().startsWith("convert")) {
-               convert(function);
+               int temp = convert(function);
+               String f_c = temp_type(function);
+               f_c = f_c.toUpperCase();
+               System.out.println(temp + f_c);
             } 
             // User range(String value) function
             else if (function.toLowerCase().startsWith("range")) {
-               range(function);
+               String result = range(function);
+               System.out.println(result);
             } 
             // Use repeat(String value) function
             else if (function.toLowerCase().startsWith("repeat")) {
-               repeat(function);
+               String new_str = repeat(function);
+               System.out.println(new_str);
             } 
             // Reprompt the user with prompt(Scanner console) function
             else if (function.toLowerCase().startsWith("end")) {
-                     System.out.println();
-                     answer = prompt(console);
+               System.out.println();
+               answer = prompt(console);
             }
-         // Answer is a case-insensitive version of "I"                        
-         } else if (answer.equalsIgnoreCase("i")) { 
-            String file_input = interpret(console);
+         // Answer is a case-insensitive version of "I"                    
+         } else if (answer.equalsIgnoreCase("i")) {  
+            String name_input = interpret(console);
+            readFile(console, name_input);
+            System.out.println();
             answer = prompt(console);         
-         } else { // Answer is a case-insensitive version of "Q" 
+         } else if (answer.equalsIgnoreCase("q")) {
             answer = "";
-         }
+         } 
       }      
    }
-   
+
    // Introduction
    public static void intro() {
-      System.out.println("Welcome to the YazInterpreter!");
-      System.out.println("You may interpret a YazLang program and output");
-      System.out.println("the results to a .txt file or enter console YazInteractions");
-      System.out.println("mode to run single commands of YazLang.");
-      System.out.println();   
+    System.out.println("Welcome to the YazInterpreter!");
+    System.out.println("You may interpret a YazLang program and output");
+    System.out.println("the results to a .txt file or enter console YazInteractions");
+    System.out.println("mode to run single commands of YazLang.");
+    System.out.println();   
    }
-   
+      
    // User Prompt for "C", "I" or "Q"
    public static String prompt (Scanner console) {
       System.out.print("(C)onsole YazInteractions, (I)nterpret .yzy program, (Q)uit? ");
-      String answer = console.next();
-      return answer;
+      String input = console.nextLine();
+      return input;
    }   
-   
+
    // Method that convert temperature between F and C
-   public static int convert (String value) {
+   public static int convert (String input) {
       int temp_convert = 0;
       // Setting up new Scanner to read the User-input string
-      Scanner token = new Scanner(value);
+      Scanner token = new Scanner(input);
       // Take out the word "CONVERT"
       String fnc = token.next();
       // Get the temperature that needs to be converted
@@ -93,94 +101,110 @@ public class YazInterpreter {
       // Current unit of temperature
       String type = token.next().toLowerCase();
       if (type.equals("f")) {
-         temp_convert = (int) Math.round((temp - 32)/1.8); 
-         System.out.println(temp_convert + "C");
+         temp_convert = (int) ((temp - 32)/1.8); 
       } else if (type.equals("c")) {
-         temp_convert = (int) Math.round(1.8 * temp + 32);
-         System.out.println(temp_convert + "F");
+         temp_convert = (int) (1.8 * temp + 32);
       }
       return temp_convert;
    }
-   
+   // Temperature type
+   public static String temp_type (String input) {
+      int temp_convert = 0;
+      // Setting up new Scanner to read the User-input string
+      Scanner token = new Scanner(input);
+      // Take out the word "CONVERT"
+      String fnc = token.next();
+      // Get the temperature that needs to be converted
+      int temp = token.nextInt();
+      // Current unit of temperature
+      String type = token.next();
+      if (type.equalsIgnoreCase("c")) {
+         type = "f";
+      } else if (type.equalsIgnoreCase("f")) {
+         type = "c";
+      }
+      return type;
+   }
+
    // Method that print a sequence of numbers
    // until arg 1 + arg 3 >= arg 2 
-   public static String range (String value) {
+   public static String range (String input) {
       // Setting up new Scanner to read the User-input string
-      Scanner token = new Scanner(value);
+      Scanner token = new Scanner(input);
       // Take out the word "RANGE"
       String fnc = token.next();
       // Read in the three interger arguments
       int arg_1 = token.nextInt();
       int arg_2 = token.nextInt();
       int arg_3 = token.nextInt();
-      String str_range = arg_1 + " ";
+      String str = arg_1 + " ";
       while (arg_1 < arg_2) {
-         System.out.print(str_range);
          arg_1 += arg_3;
-         str_range = arg_1 + " ";
+         str += arg_1 + " ";
       }
-      System.out.println();
-      return str_range;
+      return str;
    }
 
    // Method that print out string argument repeated 
    // number of times indicated by following interger
-   public static String repeat (String value) {
+   public static String repeat (String input) {
       // Setting up new Scanner to read the User-input string
-      Scanner token = new Scanner(value);
+      Scanner token = new Scanner(input);
       String fnc = token.next();
-      String str = "";
+      String b = "";
       while (token.hasNext()) {
          // Seperate the string that starts with \" and end with \"
-         str = token.next();
+         String str = token.next();
+         // String replacement
          str = str.replace("\"", "");
          str = str.replace("_", " ");
          int num = token.nextInt();
          for (int i = 1; i <= num; i++) {
-            // str = str + ... + str (number of num);
-            System.out.print(str);
-         }      
+            b += str;
+         }     
       }
-      System.out.println();
-      return str;
+      return b;
    }
-   
+
    // Method use to get file and interpret the file
    public static String interpret (Scanner console) throws FileNotFoundException {
       // Prompt user for input file's name
       System.out.print("Input file name: ");
-      String name_input = console.next();
+      String name_input = console.nextLine();
       File file_input = new File(name_input);
       while(!file_input.exists() && !name_input.contains(".yzy")) {
          System.out.print("File not found. Try again: ");
-         name_input = console.next();
+         name_input = console.nextLine();
          file_input = new File(name_input);
       }
-      readFile(console, name_input);
       return name_input;
    }   
    
-   public static void readFile (Scanner console, String file_input) throws FileNotFoundException {
+   // Read in the file and create an output file
+   public static void readFile (Scanner console, String file_input) 
+                                throws FileNotFoundException {
       // Setting up new Scanner to scan the file
-      Scanner input = new Scanner (file_input);
-       // Prompt user for output file's name
+      Scanner input = new Scanner (new File(file_input));
+      // Prompt user for output file's name
       System.out.print("Output file name: ");
-      String name_output = console.next();
-      
+      String name_output = console.nextLine();
       PrintStream file_output = new PrintStream(new File(name_output));
-      
       System.out.println("YazLang program interpreted and output to .txt file!");
-      System.out.println();
+      // Print results into .txt file
       while (input.hasNext()) {
          String value = input.nextLine().toLowerCase();
          if (value.startsWith("convert")) {
-            int convert_temp = convert(value);
-            file_output.println(convert_temp);
+            int new_temp = convert(value);
+            String f_c = temp_type(value);
+            f_c = f_c.toUpperCase();
+            file_output.println(new_temp + f_c);
          } else if (value.startsWith("range")) {
-            range(value);
+            String new_str = range(value);
+            file_output.println(new_str);
          } else if (value.startsWith("repeat")) {
-            repeat(value);
+            String new_repeat = repeat(value);
+            file_output.println(new_repeat);
          }
       }
-   }     
+   }    
 }
